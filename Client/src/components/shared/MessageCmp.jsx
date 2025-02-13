1679a393f2500b3e1fab12c1b9c9bd859a2aef89
Hasan Coder 66 <hasanashraf874@gmail.com>
2025-02-14 00:35:41 +0500

    3:38:20

--- Client/src/components/shared/MessageCmp.jsx
+++ Client/src/components/shared/MessageCmp.jsx
@@ -1,10 +1,14 @@
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import moment from "moment";
 import React, { memo } from "react";
+import { fileFormat } from "../../lib/features";
+import RenderAttatchment from "./RenderAttatchment";
 
 const MessageCmp = ({ message, user }) => {
-  const { content, createdAt, sender, attactment = [] } = message;
+  const { content, createdAt, sender, attactments = [] } = message;
 
   const sameSender = sender?._id === user?._id;
+  const timeAgo = moment(createdAt).fromNow();
   return (
     <div
       style={{
@@ -17,8 +21,40 @@ const MessageCmp = ({ message, user }) => {
       }}
     >
       {/* {content} */}
-      {!sameSender && <Typography color="#2694ab" fontWeight={"600"} variant="caption"> {sender?.name}</Typography>}
-      {!content && <Typography> {content}</Typography>}
+      {!sameSender && (
+        <Typography color="#2694ab" fontWeight={"600"} variant="caption">
+          {" "}
+          {sender?.name}
+        </Typography>
+      )}
+      {content && <Typography> {content}</Typography>}
+
+      {/* Attatchment  */}
+      {attactments.length > 0 &&
+        attactments.map((attactment, index) => {
+          const url = attactment.url;
+          const file = fileFormat(url);
+
+          return (
+            <Box key={index}>
+              <a
+                target="_blank"
+                href=""
+                download
+                style={{
+                  color: "black",
+                }}
+              >
+
+                {RenderAttatchment(file, url)}
+              </a>
+            </Box>
+          );
+        })}
+
+      <Typography variant="caption" color="text.secondary">
+        {timeAgo}
+      </Typography>
     </div>
   );
 };
---
+++ Client/src/components/shared/RenderAttatchment.jsx
@@ -0,0 +1,30 @@
+import { FileOpen as FileOpenIcon } from "@mui/icons-material";
+import React from "react";
+
+const RenderAttatchment = (file, url) => {
+  switch (file) {
+    case "video":
+      return <video src={url} preload="none" width={"200px"} controls />;
+
+    case "image":
+      return (
+        <img
+          src={transformImage(url, 200)}
+          alt="Attactment"
+          width={"200"}
+          height={"150"}
+          style={{
+            objectFit: "contain",
+          }}
+        />
+      );
+
+    case "audio":
+      return <audio src={url} preload="none" controls />;
+
+    default:
+      return <FileOpenIcon />;
+  }
+};
+
+export default RenderAttatchment;
